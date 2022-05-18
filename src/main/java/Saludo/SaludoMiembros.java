c3f455e2e31615c3e941d032d16c7f97f75d489a
LENOVO <papux45@gmail.com>
2022-05-18 13:58:13 -0500

    Bienvenida

---
+++ src/main/java/Saludo/SaludoMiembros.java
@@ -0,0 +1,8 @@
+package Saludo;
+
+public class SaludoMiembros {
+    public static void main(String[] args) {
+        String saludo = "BIENVENIDOS A COMPUTER SOCIETY :)";
+        System.out.println(saludo);
+    }
+}
